1fa7bbfe1e162494353088cb99306eb644cee382
ChangedDarkDex <104589490+ChangedDarkDex@users.noreply.github.com>
2022-04-28 20:26:17 +0200

    Update and rename notification.lua to car_script.lua

--- car_script.lua
+++ car_script.lua
@@ -1,6 +1,8 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/MonkoTubeYT/carscript/master/!carscript.lua',true))()
+
 game:GetService("StarterGui"):SetCore("SendNotification", { 
 	Title = "Notification";
-	Text = "Mama mia! -Merio";
+	Text = "Car loaded!";
 	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
   	Button1 = "Yes";
 Duration = 16;
