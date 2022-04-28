8293aade9bd0dd96d338334222392eba522b45bc
ChangedDarkDex <104589490+ChangedDarkDex@users.noreply.github.com>
2022-04-28 20:03:13 +0200

    Create notification.lua

---
+++ notification.lua
@@ -0,0 +1,6 @@
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Notification";
+	Text = "Mama mia! -Merio";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+  Button1 = "Yes";
+Duration = 16;
