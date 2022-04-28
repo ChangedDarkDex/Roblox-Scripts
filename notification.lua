2a41491a24bb0b856af1ffe91bcf7f7bf0fcfd00
ChangedDarkDex <104589490+ChangedDarkDex@users.noreply.github.com>
2022-04-28 20:21:47 +0200

    Update notification.lua

--- notification.lua
+++ notification.lua
@@ -2,5 +2,5 @@ game:GetService("StarterGui"):SetCore("SendNotification", {
 	Title = "Notification";
 	Text = "Mama mia! -Merio";
 	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
-  Button1 = "Yes";
+  	Button1 = "Yes";
 Duration = 16;
